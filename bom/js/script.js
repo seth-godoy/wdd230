9acff8fc94d15822d0707a5453274d6c581ba62b
seth-godoy <u443447760@byui.edu>
2022-05-21 17:35:04 -0600

    Added Book of Mormon: Favorite Chapters

---
+++ bom/js/script.js
@@ -0,0 +1,24 @@
+const favchap = document.getElementById('favchap');
+const addchap = document.getElementById('addchap');
+const listchap = document.getElementById('listchap');
+
+addchap.addEventListener('click', function() {
+    const chap = favchap.value;
+    favchap.value = '';
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    listText.textContent = chap;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = '❌';
+    listchap.appendChild(listItem);
+
+    listBtn.addEventListener('click', function () {
+        listchap.removeChild(listItem);
+    });
+
+    favchap.focus();
+});
