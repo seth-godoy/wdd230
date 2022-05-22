a56a23d4c492c87a58bb97a61e4490d0dd0782dc
seth-godoy <u443447760@byui.edu>
2022-05-21 20:54:20 -0600

    Added banner

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -24,3 +24,15 @@ function toggleMenu() {
 
 const x = document.getElementById('navBtn');
 x.onclick = toggleMenu;
+
+// Monday-Tuesday Banner
+const dayNumber = now.getDay();
+const banner = document.querySelector('#banner');
+
+if (dayNumber == 1) {
+    banner.classList.add('showme');
+} else if (dayNumber == 2) {
+    banner.classList.add('showme');
+} else {
+    banner.classList.add('hideme');
+};
