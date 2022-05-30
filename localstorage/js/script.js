2e8c004c63306856bad907751374909c3cd5657b
seth-godoy <u443447760@byui.edu>
2022-05-30 15:34:05 -0600

    Added comments to js file

--- localstorage/js/script.js
+++ localstorage/js/script.js
@@ -1,10 +1,11 @@
-// Demonstration of localStorage
+// Store HTML elements in variables
 const displayName = document.getElementById('displayName');
 const enterName = document.getElementById('enterName');
 const saveName = document.getElementById('saveName');
 const showName = document.getElementById('showName');
 const deleteName = document.getElementById('deleteName');
 
+// Click event on Save Name button to store name in Storage
 saveName.addEventListener('click', function() {
     let myName = enterName.value;
     enterName.value = '';
@@ -12,10 +13,12 @@ saveName.addEventListener('click', function() {
     enterName.focus();
 });
 
+// Click event on Show Name button to show the stored name
 showName.addEventListener('click', function() {
     displayName.textContent = localStorage.getItem('name');
 });
 
+// Click event on Delete Name button to delete all data in Storage
 deleteName.addEventListener('click', function() {
     localStorage.clear();
 });
